00f694629e781f01c3e98c938007c8b33d88d9d9
Owais Shariff <osh.econnect@gmail>
2023-10-30 16:33:01 +0300

    Frontend Bug Fix

--- src/components/landingsections/HeroSection.jsx
+++ src/components/landingsections/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             
             <div class="mr-auto place-self-center lg:col-span-7">
-            <a href="#" target='_blank' class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700  rounded-full bg-secondary dark:bg-darksecondary dark:text-darktext hover:bg-highlight dark:hover:bg-gray-700" role="alert">
+            <a href="#" target='_blank' class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700  rounded-full bg-secondary dark:bg-darksecondary dark:text-darktext hover:bg-highlight dark:hover:bg-darkhighlight" role="alert">
                 <span class="text-xs bg-primary dark:bg-darkprimary   rounded-full text-darktext px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Blog released! Read on Medium</span> 
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
             </a>
