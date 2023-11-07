e12e3bc6e8eb6cf5bc7757217886facd1631ea10
Owais Shariff <osh.econnect@gmail>
2023-11-07 22:14:29 +0300

    Bug Fix

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -6,81 +6,65 @@ import EventList from "../../components/events/Events"
 export default function Events() {
   const EventData = [
     {
-      "event": "Old Katpadi School Visit",
-      "cover": "/assets/herosec.png",
-      "date": "Nov 6, 2023",
-      "location": "Google Meet",
-      "time": "",
-      "description": "In the heart of VIT Vellore, we found ourselves drawn to a club of with a mission that transcended conventional teaching...",
-      "url": "https://medium.com/becoming-i-foundation/bif-becoming-i-foundation-bridging-the-educational-gap-and-spreading-hope-online-782bd5c0dd25"
+      "eventname": "Old Katpadi School Visit",
+      "date": "Nov 1, 2023",
+      "location": "Offline Session",
+      "time": "11:30 AM",
+      "cover": "https://raw.githubusercontent.com/BIFVIT/bif-website/main/src/assets/herosec.jpg",
+      "description": "We organized offline sessions focused on providing knowledge-based interactive sessions for underprivileged children from Old Katpad...",
+      "url": "/events/past/old-katpadi-school-visit"
     },
     {
-      "author": "St Judes Visit",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/0*aUP2KIHHGFhPEWx7",
-      "date": "Oct 25, 2023",
-      "readTime": "2 min read",
-      "title": "Bridging the Educational Gap and Spreading Hope Online",
-      "description": "In the heart of VIT Vellore, we found ourselves drawn to a club of with a mission that transcended conventional teaching...",
-      "url": "https://medium.com/becoming-i-foundation/bif-becoming-i-foundation-bridging-the-educational-gap-and-spreading-hope-online-782bd5c0dd25"
+        "eventname": "St Judes Childcare Centre Visit",
+        "date": "May 17, 2023",
+        "location": "Offline Session",
+        "time": "",
+        "cover": "/stjudesvisit.jpg",
+        "description": "We shared an endearing day with the resilient kids facing the challenges of cancer. Our visit was filled with fun art activities, ed...",
+        "url": "/events/past/st-judes-visit"
     },
     {
-      "author": "Dhruv Mathur",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/0*R4uiZQFmuGWBIU4t",
-      "date": "Jan 2, 2023",
-      "readTime": "3 min read",
-      "title": "New year, New resolutions, New beginning.",
-      "description": "It's a myth that you can't have it all. You can have it all just maybe not all at the…",
-      "url": "https://medium.com/becoming-i-foundation/new-year-new-resolutions-new-beginning-20fd50f01ad3"
+        "eventname": "Hack4Impact (ft. IEEECS)",
+        "date": "Jun 6, 2023 - Jun 7, 2023",
+        "location": "CS Hall",
+        "time": "9:00 AM",
+        "cover": "https://raw.githubusercontent.com/BIFVIT/bif-website/main/src/assets/herosec.jpg",
+        "description": "One of the major tech events of Yantra in collaboration with IEEE-CS. The hackathon saw 30+ teams competing for UNICEF's SDG Goal 4... ",
+        "url": "/events/past/hack4impact"
     },
     {
-      "author": "Dhruv Mathur",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/0*R4uiZQFmuGWBIU4t",
-      "date": "Oct 10, 2022",
-      "readTime": "5 min read",
-      "title": "Say yes to positivity, say yes to well being",
-      "description": "Amidst the recent pandemic, everyone understood the value...",
-      "url": "https://medium.com/becoming-i-foundation/anything-thats-human-is-mentionable-and-anything-that-is-mentionable-can-be-more-manageable-a5dd9e8f01cd"
+        "eventname": "Human Snakes & Ladders",
+        "date": "Feb 26, 2023",
+        "location": "Foodys",
+        "time": "10:00 AM",
+        "cover": "https://raw.githubusercontent.com/BIFVIT/bif-website/main/src/assets/herosec.jpg",
+        "description": "One of the most sought-after events in Riviera 22. With over 750+ registrations, it was a fun game that was played on a life-sized sn...",
+        "url": "/events/past/human-snakes-and-ladders"
     },
     {
-      "author": "Dhruv Mathur",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/0*R4uiZQFmuGWBIU4t",
-      "date": "Sep 29, 2022",
-      "readTime": "4 min read",
-      "title": "Preceding wastage counts for imminent shortage",
-      "description": "In a world of seven billion people, set to grow to nine billion by 2050...",
-      "url": "https://medium.com/becoming-i-foundation/preceding-wastage-counts-for-imminent-shortage-cccb7cd00429"
+        "eventname": "Smile",
+        "date": "Feb 24, 2023",
+        "location": "SJT Gallery",
+        "time": "10:00 AM",
+        "cover": "https://raw.githubusercontent.com/BIFVIT/bif-website/main/src/assets/herosec.jpg",
+        "description": "A Day of Fun & Entertainment for Underprivileged Kids in VIT. From music & games to comedy and dance, we shared laughter and joy, cre... ",
+        "url": "/events/past/smile"
     },
-    {
-      "author": "Satyanidhi",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/1*dmbNkD5D-u45r44go_cf0g.png",
-      "date": "Jul 29, 2022",
-      "readTime": "4 min read",
-      "title": "Nurture Nature for a Nurturing Future",
-      "description": "The seven billion humans inhabit the earth and use various amounts of the planet's resources...",
-      "url": "https://medium.com/becoming-i-foundation/nurture-nature-for-a-nurturing-future-2ad13f13daa6"
-    },
-    {
-      "author": "Dhruv Mathur",
-      "avatar": "https://miro.medium.com/v2/resize:fill:72:72/0*R4uiZQFmuGWBIU4t",
-      "date": "Jun 4, 2022",
-      "readTime": "4 min read",
-      "title": "Take a stand, make a difference!",
-      "description": "When we save children, we save ourselves. The solution of adult problems…",
-      "url": "https://medium.com/becoming-i-foundation/take-a-stand-make-a-difference-de616898afd7"
-    }
+
+    
   ]
   
 
   return (
-    <section class="bg-background dark:bg-darkbackground">
+    <section class="h-full bg-background dark:bg-darkbackground">
       <Navbar />
       <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 bg-background dark:bg-darkbackground">
         <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
           <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-primary dark:text-darkprimary">
-            Our Blog
+            Our Events
           </h2>
           <p class="font-light text-text sm:text-xl dark:text-darktext">
-          Explore our blog for stories, insights, and updates that showcase the heart of our mission. 
+          Explore the many initiatives & events we&apos;ve organized to do good in our community and beyond. We&apos;d love for you to check out our exciting work and get involved in something meaningful.
           </p>
         </div>
         <div class="container">
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { MdLocationOn } from "react-icons/md"
 
-const ArticleCard = ({ author, avatar, date, readTime, title, description, url }) => {
+const EventCard = ({ eventname, date, location, time, cover, description, url }) => {
     const router = useRouter();
 
     const redirectToUrl = (url) => {
@@ -13,66 +14,92 @@ const ArticleCard = ({ author, avatar, date, readTime, title, description, url }
     };
 
   return (
-    <article onClick={()=>redirectToUrl(`${url}`)} className="flex flex-col p-6 bg-white rounded-lg bg-secondary  dark:bg-darksecondary dark:border-gray-700 hover:scale-105 transition duration-500 cursor-pointer object-cover">
-      <div className="flex justify-between items-center mb-5 text-gray-500">
-        <span className="bg-highlight text-primary dark:bg-darkhighlight dark:text-darkprimary text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
-          <svg
-            className="mr-1 w-3 h-3"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-              clipRule="evenodd"
-            ></path>
-            <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-          </svg>
-          {readTime}
-        </span>
-        <span className="text-text dark:text-darktext text-sm">{date}</span>
-      </div>
-      <div className='flex-grow'>
-      <h2 className="mb-2 text-2xl font-bold tracking-tight text-text opacity-80 dark:text-darktext">
-        <a href={url} target='_blank'>{title}</a>
-      </h2>
-      <p className="mb-5 font-light text-text dark:text-darktext">
-        {description}
-      </p>
-      </div>
-      <div className="mt-5 flex-end my-auto flex justify-between items-center">
-        <div className=" flex items-center space-x-4">
-          <Image
-            width={28}
-            height={28}
-            src={avatar}
-            alt="Author avatar"
-            className="w-7 h-7 rounded-full"
-          />
-          <span className="font-medium dark:text-darktext">{author}</span>
+    // <article onClick={()=>redirectToUrl(`${url}`)} className="flex flex-col p-6 bg-white rounded-lg bg-secondary  dark:bg-darksecondary dark:border-gray-700 hover:scale-105 transition duration-500 cursor-pointer object-cover">
+      
+    //   <div className="flex justify-between items-center mb-5 text-gray-500">
+    //     <span className="bg-highlight text-primary dark:bg-darkhighlight dark:text-darkprimary text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+    //     <MdLocationOn className="mr-1 w-3 h-3"/>
+    //       {location}
+    //     </span>
+    //     <span className="text-text dark:text-darktext text-sm">{date}</span>
+    //   </div>
+    //   <div className='flex-grow'>
+    //   <h2 className="mb-2 text-2xl font-bold tracking-tight text-text opacity-80 dark:text-darktext">
+    //     <a href={url} target='_blank'>{eventname}</a>
+    //   </h2>
+    //   <p className="mb-5 font-light text-text dark:text-darktext">
+    //     {description}
+    //   </p>
+    //   </div>
+    //   <div className="mt-5 flex-end my-auto flex justify-between items-center">
+    //     <span><p> </p></span>
+    //     <a
+    //       href={url} 
+    //       target='_blank'
+    //       className="inline-flex items-center font-medium text-primary dark:text-darkprimary hover:underline"
+    //     >
+    //       Read more
+    //       <svg
+    //         className="ml-2 w-4 h-4"
+    //         fill="currentColor"
+    //         viewBox="0 0 20 20"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //       >
+    //         <path
+    //           fillRule="evenodd"
+    //           d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+    //           clipRule="evenodd"
+    //         ></path>
+    //       </svg>
+    //     </a>
+    //   </div>
+    // </article>
+    //  className="flex flex-col p-6 rounded-lg bg-secondary  dark:bg-darksecondary dark:border-gray-700 hover:scale-105 transition duration-500 cursor-pointer object-cover">
+    
+    <div onClick={()=>redirectToUrl(`${url}`)} class="rounded-lg bg-secondary  dark:bg-darksecondary hover:scale-105 transition duration-500 cursor-pointer  max-w-sm w-full lg:max-w-full lg:flex">
+        <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-lg  text-center overflow-hidden" style={{ backgroundImage: `url('${cover}'),  backgroundPosition: 'left'` }} title="Woman holding a mug">
         </div>
-        <a
-          href={url} 
-          target='_blank'
-          className="inline-flex items-center font-medium text-primary dark:text-darkprimary hover:underline"
-        >
-          Read more
-          <svg
-            className="ml-2 w-4 h-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </a>
-      </div>
-    </article>
+        <div class="border-r border-b border-l lg:border-l-0 lg:border-t bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div class="mb-8">
+            <div className="flex justify-between items-center mb-5 text-gray-500">
+                <span className="bg-highlight text-primary dark:bg-darkhighlight dark:text-darkprimary text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                <MdLocationOn className="mr-1 w-3 h-3"/>
+                {location}
+                </span>
+                <span className="text-text dark:text-darktext text-sm">{date}</span>
+            </div>
+            <h2 className="mb-2 text-2xl font-bold tracking-tight text-text opacity-80 dark:text-darktext">
+                <a href={url} target='_blank'>{eventname}</a>
+            </h2>
+            <p className="mb-2 font-light text-text dark:text-darktext">
+                {description}
+            </p>
+            </div>
+            <div className="flex-end my-auto flex justify-between items-center">
+                <span><p> </p></span>
+                <a
+                href={url} 
+                target='_blank'
+                className="inline-flex items-center font-medium text-primary dark:text-darkprimary hover:underline"
+                >
+                Read more
+                <svg
+                    className="ml-2 w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                    ></path>
+                </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+
   );
 };
 
@@ -80,7 +107,7 @@ const EventList = ({ data }) => {
   return (
     <div className="grid gap-8 lg:grid-cols-2">
       {data.map((article, index) => (
-        <ArticleCard key={index} {...article} />
+        <EventCard key={index} {...article} />
       ))}
     </div>
   );
